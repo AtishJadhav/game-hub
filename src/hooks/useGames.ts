22c53b547fd61c6d61e172819c92183f6dc45904
Atish <atish.jadhav@crmnext.com>
2026-09-13 12:49:26 +0530

    Getting Optimized Images - 2

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-import { number } from "framer-motion";
 
 export interface Platform {
     id: number;
